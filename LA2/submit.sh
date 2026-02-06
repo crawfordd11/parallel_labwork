ffcf5834674c549b2c8a3e32d161f4f8815a091a
crawfordd1 <crawfordd1@wit.edu>
2026-02-06 10:29:50 -0500

    miscalculated hops in pingpong

--- LA2/submit.sh
+++ LA2/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=program
 #SBATCH --partition=batch
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:00:15
 #SBATCH --output=output_program.%j
