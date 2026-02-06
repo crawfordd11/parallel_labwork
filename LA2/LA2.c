38cc0c42de88ea2fca807ea408f86274bba5ffe6
cscrawford <carmensleight@gmail.com>
2026-02-06 09:38:56 -0500

    Clean print statements

--- LA2/LA2.c
+++ LA2/LA2.c
@@ -26,7 +26,6 @@ int main(int argc, char **argv){
         for(int i = 1; i<numranks; i++){
             MPI_Send(numbers,size,MPI_INT,i,0,MPI_COMM_WORLD);
             MPI_Recv(numbers,size,MPI_INT,i,0,MPI_COMM_WORLD,&stat);
-            printf("Rank %d\n",rank);
         }
         
         end_time = clock();
@@ -37,7 +36,6 @@ int main(int argc, char **argv){
     }
     else{
         MPI_Recv(numbers,size,MPI_INT,0,0,MPI_COMM_WORLD,&stat);
-        printf("Rank %d", rank);
         numbers[rank] = rank;
         MPI_Send(numbers,size,MPI_INT,0,0,MPI_COMM_WORLD);
     }
@@ -49,17 +47,16 @@ int main(int argc, char **argv){
         
         MPI_Send(numbers,size,MPI_INT,1,0,MPI_COMM_WORLD);
         MPI_Recv(numbers,size,MPI_INT,numranks-1,0,MPI_COMM_WORLD,&stat);
-        printf("Rank %d\n",rank);
         
         
         end_time = clock();
 
         cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
         printf("Execution time: %f seconds\n", cpu_time_used);
+        printf("Execution time average: %f seconds\n", (cpu_time_used/(numranks*2)));
     }
     else{
         MPI_Recv(numbers,size,MPI_INT,rank-1,0,MPI_COMM_WORLD,&stat);
-        printf("Rank %d", rank);
         numbers[rank] = rank;
         MPI_Send(numbers,size,MPI_INT,(rank+1)%numranks,0,MPI_COMM_WORLD);
     }
