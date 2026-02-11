375f842a5d40cb9559c72671ca96250b766c8e3a
crawfordd11 <crawfordd1@wit.edu>
2026-02-10 22:56:08 -0500

    review assignB

--- LA3/LA3.c
+++ LA3/LA3.c
@@ -162,7 +162,7 @@ int main(int argc, char** argv){
     //generate data
     //long SIZE = 1024*1024*1024/sizeof(double); //1GB worth of doubles
 
-    int n = 3, m = 3, p = 3; // Example dimensions to match assingment
+    int n = 2, m = 3, p = 4; // Example dimensions to match assingment
     double* A = (double*) malloc(n * m * sizeof(double));
     double* B = (double*) malloc(m * p * sizeof(double));    
     double* C = (double*) malloc(n * p * sizeof(double)); 
