c2a9f7ecfbf3c127784442ab7b7b2996f8fa6cf5
crawfordd11 <crawfordd1@wit.edu>
2026-02-10 22:07:33 -0500

    originate submit.sh

---
+++ LA3/submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=LA3bcastTest
+#SBATCH --partition=batch
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:00:15
+#SBATCH --output=output_program.%j
+#SBATCH --export=ALL
+
+srun ./LA2.x
