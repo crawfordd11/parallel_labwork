cb3d4cfd78938477c9be07df09b456a77bd0f694
crawfordd11 <crawfordd1@wit.edu>
2026-03-16 21:35:46 -0400

    cleanup comments

--- LA4/LA4.c
+++ LA4/LA4.c
@@ -12,7 +12,6 @@ int main( int argc, char** argv ) {
     MPI_Status stat;
 
     double startfull=MPI_Wtime();
-
     int *matrix;
     int *temp;
     char *name="image.jpg";
@@ -20,45 +19,28 @@ int main( int argc, char** argv ) {
    
     //read image
     if(rank==0){
-        printf("Reading image...\n");
-        
+        //printf("Reading image...\n");
         matrix=imageToMat(name,dims);
     }
     
     //broadcast dimensions and image
     double startbcast=MPI_Wtime();
     
-    printf("Broadcasting image...\n");
-
+    //printf("Broadcasting image...\n");
     MPI_Bcast(dims,2,MPI_INT,0,MPI_COMM_WORLD);
-
-    /* refactor later
-        int height=dims[0];
-        int width=dims[1];
-    */
-
     if(rank!=0) matrix = (int*) malloc(dims[0]*dims[1]*sizeof(int));
-
     MPI_Bcast(matrix,dims[0]*dims[1],MPI_INT,0,MPI_COMM_WORLD);
 
     double endbcast=MPI_Wtime();
 
-    //initalize ranks and calculate convolution
+    
     double startcalc=MPI_Wtime();
-
-    printf("Calculating convolution...\n");
-
+    //printf("Calculating convolution...\n");
     //check for proper number of processes
     if(dims[0]%numranks !=0){
         if(rank==0) printf("Error: Height does not divide evenly by number of processes.\n");
-        
         MPI_Abort(MPI_COMM_WORLD, 1);
-        // free(dims);
-        // free(matrix);
-        // MPI_Finalize();
-        // return 0;
     }
-
 
     int height=dims[0];
     int width=dims[1];  
@@ -68,10 +50,8 @@ int main( int argc, char** argv ) {
     int myRowEnd=(rank+1)*numrows;
     if(rank==numranks-1) myRowEnd=dims[0];
 
-    
-
     //create kernel size history: 51
-    int size=3;
+    int size=51;
     int range=size/2;
     int k=range;//consider removing later, but it makes the code more readable
     double *gKernel[size];
@@ -110,9 +90,7 @@ int main( int argc, char** argv ) {
     
 
     double startgather=MPI_Wtime();
-
-    printf("Gathering results...\n");
-
+    //printf("Gathering results...\n");
     //mpi gather
     MPI_Gather(temp,numrows*width,MPI_INT,matrix,numrows*width,MPI_INT,0,MPI_COMM_WORLD);
     //save image
@@ -120,14 +98,16 @@ int main( int argc, char** argv ) {
 
     double endgather=MPI_Wtime();
 
-    //cleanup
+
     double endfull=MPI_Wtime();
 
-    printf("Number of Ranks: %d\n",numranks);
-    printf("Full time: %f\n",endfull-startfull);
-    printf("Bcast time: %f\n",endbcast-startbcast);
-    printf("Calc time: %f\n", endcalc-startcalc);
-    printf("Gather time: %f\n",endgather-startgather);  
+    if(rank==0){
+        printf("Number of Ranks: %d\n",numranks);
+        printf("Full time: %f\n",endfull-startfull);
+        printf("Bcast time: %f\n",endbcast-startbcast);
+        printf("Calc time: %f\n", endcalc-startcalc);
+        printf("Gather time: %f\n",endgather-startgather);  
+    }
 
     free(dims);
     free(matrix);
