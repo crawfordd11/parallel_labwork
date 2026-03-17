8f84c4f0545236d38c5ae9f8910bee5470638999
crawfordd11 <crawfordd1@wit.edu>
2026-03-16 20:38:01 -0400

    fix declare endfull before call

--- LA4/LA4.c
+++ LA4/LA4.c
@@ -123,18 +123,18 @@ int main( int argc, char** argv ) {
 
     //cleanup    
 
-    printf("Number of Ranks: %d\n",numranks);
-    printf("Full time: %f\n",endfull-startfull);
-    printf("Bcast time: %f\n",endbcast-startbcast);
-    printf("Calc time: %f\n", endcalc-startcalc);
-    printf("Gather time: %f\n",endgather-startgather);  
-
     free(dims);
     free(matrix);
     free(temp);
     for(int i=0; i<size; i++) free(gKernel[i]);
 
+    printf("Number of Ranks: %d\n",numranks);
     double endfull=MPI_Wtime();
+    printf("Full time: %f\n",endfull-startfull);
+    printf("Bcast time: %f\n",endbcast-startbcast);
+    printf("Calc time: %f\n", endcalc-startcalc);
+    printf("Gather time: %f\n",endgather-startgather);  
+
     MPI_Finalize();
 
     return 0;
