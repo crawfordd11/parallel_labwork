0497b4deb2ae8bd353dc1e604746332e3cfbb159
crawfordd1 <crawfordd1@wit.edu>
2026-03-19 21:15:52 -0400

    smallerBox data gather 11 px

--- LA4/output/simpleBoxBlur/submit.sh
+++ LA4/output/simpleBoxBlur/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=LA4Convolution51
 #SBATCH --partition=batch
-#SBATCH --nodes=1
+#SBATCH --nodes=12
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:00:30
 #SBATCH --output=out_simpleBoxBlur.%j
