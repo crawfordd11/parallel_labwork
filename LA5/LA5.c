638567d03ed624cd2c004d33e242b50f4a07d4b0
cscrawford <carmensleight@gmail.com>
2026-03-23 13:49:39 -0400

    starter lab with 6k+-1 optimization

---
+++ LA5/LA5.c
@@ -0,0 +1,38 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+int is_prime(int n);
+
+int main(int argc, char** argv){
+
+    int n=10000000;
+    int numprimes = 0;
+    int i;
+    for (i = 5; i <= n; i+=6)
+    {
+        if (is_prime(i) == 1) numprimes ++;
+        if (is_prime(i+2) == 1) numprimes ++;
+    }
+
+    printf("Number of Primes: %d\n",numprimes);
+
+}
+
+int is_prime(int n)
+{
+    /* handle special cases */
+    if      (n == 0) return 0;
+    else if (n == 1) return 0;
+    else if (n == 2) return 1;
+    else if (n == 3) return 1;
+    else if (n % 2 == 0) return 0;
+    else if (n % 3 == 0) return 0;
+
+    int i;
+    for(i=5;i<=(int)(sqrt((double) n));i+=6)
+        if (n%i==0) return 0;
+        if (n%(i+2)==0) return 0;
+    return 1;
+}
