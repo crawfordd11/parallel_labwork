2666c678b90ae2ff4cf7c2a2881a315eb791b93a
crawfordd1 <crawfordd1@wit.edu>
2026-03-26 23:20:54 -0400

    pesudo optimal chunk for all ranks

--- LA5/submit.sh
+++ LA5/submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=program
 #SBATCH --partition=batch
 #SBATCH --exclude=sc3
-#SBATCH --nodes=1
+#SBATCH --nodes=10
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:00:10
 #SBATCH --output=output_program.%j
