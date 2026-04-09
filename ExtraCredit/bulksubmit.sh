583dacbfb08c44e60c5bbd02b5d343e7b41bc3a2
crawfordd11 <crawfordd1@wit.edu>
2026-04-09 14:18:33 -0400

    organize and edit bulksubmit.sh

--- ExtraCredit/bulksubmit.sh
+++ ExtraCredit/bulksubmit.sh
@@ -2,7 +2,7 @@
 
 SIZE_MODIFIER=20020
 
-for ranks in $(seq 1 12); do
+for ranks in $(seq 2 12); do
     sbatch --job-name=DaltonMandelbrot_r${ranks} \
            --nodes=${ranks} \
            --export=ALL,SIZE_MODIFIER=${SIZE_MODIFIER} \
