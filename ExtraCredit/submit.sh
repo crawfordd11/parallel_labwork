a962f5e222df1bc058e20bbe652464c304c4e197
crawfordd11 <crawfordd1@wit.edu>
2026-04-09 15:03:01 -0400

    change submit to use 8 nodes

--- ExtraCredit/submit.sh
+++ ExtraCredit/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=LeonMandelbrot
 #SBATCH --partition=batch
-#SBATCH --nodes=2
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=00:00:20
